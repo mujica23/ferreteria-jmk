96e056f711930635a32fc4cc662b95864325aaba
Miguel Mujica <amujica64@gmail.com>
2026-02-21 13:53:47 -0600

    Fix broken db.js syntax

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
-MYSQL_DATABASErequire("dotenv").config();
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQL_DATABASE, // ← aquí está la corrección
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
   if (err) {
     console.error("Error conectando a MySQL:", err);
+    process.exit(1);
   } else {
     console.log("MySQL conectado correctamente 🚀");
   }
