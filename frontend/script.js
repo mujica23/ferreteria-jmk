a9cb74c89bdf9696e978f1ac9f998bf712b5e4f6
Miguel Mujica <amujica64@gmail.com>
2026-02-21 09:51:01 -0600

    Actualizado API a producción

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000";
+const API = "https://TU-DOMINIO.up.railway.app";
 
 /* ================= VARIABLES ================= */
 const productosDiv = document.getElementById("productos");
